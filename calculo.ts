5d058d51f4904997e640cfc4e81c8503ebd51754
sergio <oeternothanos@gmail.com>
2026-04-30 16:18:58 -0300

    meus primeiros arquivos

---
+++ calculo.ts
@@ -0,0 +1,6 @@
+// Estrutura do for: (valor inicial/ condição de parada/ passo)
+for (let i = 10; i >= 0; i--) {
+        console.log(i)
+     }
+
+console.log("Serjão berranteiro, mais conhecido como matador de onça")
